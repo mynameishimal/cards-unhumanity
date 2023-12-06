d620fa15459c7f638490c32e2eba324a9c09bbc7
Cargeek9 <140917615+Cargeek9@users.noreply.github.com>
2023-12-06 07:01:18 +0000

    added comments throughout

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 
+# homepage
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+# deleteeeeeee????
 @app.route('/cards')
 def cards():
     return render_template('cards.html')
@@ -33,43 +34,49 @@ def hash_password(password):
 # Route for user registration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
+    if request.method == 'POST':        # stores data from user form input
         username = request.form['username']
         password = request.form['password']
-        hashed_password = hash_password(password)
-
+        hashed_password = hash_password(password)   # hashes password for security
+        
+        # logs - do we remove?
         print("Username:", username)
         print("Hashed Password:", hashed_password)
 
+        # connect to user database
         conn = sqlite3.connect('new_user_database.db')
         cursor = conn.cursor()
 
+        # adds new user's data to the database
         try:
             cursor.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", (username, hashed_password))
             conn.commit()
             conn.close()
             return redirect(url_for('login'))
+        # handles error if registration fails
         except sqlite3.Error as e:
             print("SQLite error:", e)
             conn.rollback()  # Rollback any changes if there's an error
             conn.close()
             return "Error occurred while registering"
-
+    # displays registration page if not reached by POST
     return render_template('register.html')
 
-
+# login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST':        # stores data from user input
         username = request.form['username']
         password = request.form['password']
         hashed_password = hash_password(password)
 
+        # connect to user database
         conn = sqlite3.connect('new_user_database.db')
         cursor = conn.cursor()
 
+        # attempts to fetch corresponding user's data from database
         cursor.execute("SELECT * FROM users WHERE username = ? AND password_hash = ?", (username, hashed_password))
-        user = cursor.fetchone()
+        user = cursor.fetchone()    # true if user matches, false if not found
 
         conn.close()
 
@@ -78,11 +85,12 @@ def login():
             session['username'] = username
             return render_template("dashboard.html", username=username)
         else:
+            # authentication failed
             return "Invalid credentials"
-
+    # displays login page if authentication failed or not reached by POST
     return render_template('login.html')
 
-
+# display user's dashboard
 @app.route('/dashboard')
 def dashboard():
     # Check if the user is logged in before rendering the dashboard
@@ -91,54 +99,59 @@ def dashboard():
         # Fetch user data or perform necessary operations for the dashboard
         return render_template('dashboard.html', username=username)
     else:
+        # if user not logged in, redirect to login page
         return redirect(url_for('login'))
 
-
+# log out!
 @app.route('/logout')
 def logout():
     # Clear the session and log the user out
     session.pop('username', None)
     return redirect(url_for('login'))
 
-
+# removeeeeee???????
 @app.route('/cards')
 def get_cards():
     # cards = cards_collection.find()  # Retrieve cards from MongoDB
     return render_template('cards.html', cards=cards)
 
-
+# removeeee??????????
 # Needs Better implementation!! Dummy function for now
 def get_prompts():
     return prompts
 
 
-"""NOTE TO SELF TO RESET DATA AT THE BEGINNING OF EVERY SESSION!!!"""
+"""NOTE TO SELF TO RESET DATA AT THE BEGINNING OF EVERY SESSION!!!"""   # LOL REMOVE???
 
-
+# displays questions as user plays
 @app.route('/question')
+# function to display a question and answer choices
 def display_question():
     prompt_num = session.get('prompt_num', 1)  # Retrieve prompt_num from the session or default to 0
-    prompt_list = random.sample(prompts, 11)
+    prompt_list = random.sample(prompts, 11)    # randomly picks 10 prompts from list stored in prompts_manager
+    # moves to next prompt if game is not complete
     if 0 <= prompt_num < len(prompt_list):
         prompt = prompt_list[prompt_num]
         next_prompt_num = prompt_num + 1  # Increment prompt_num for the next question
+    # stores game data if game is complete
     else:
         print("DONE")
         my_game_data = session.get("game_data")
         results = end_game(my_game_data)
         stats = results[0]
         total = results[1]
+        # takes user to results page
         return render_template('game_data.html', game_data=stats, total_score=total)
 
-    # Gets cards
+    # Gets cards (responses)
     cards = sample_card_list
-    drawn_cards = random.sample(cards, 6)
-    print(drawn_cards)
+    drawn_cards = random.sample(cards, 6)      # randomly picks 6 responses from list stored in cards_manager
+    print(drawn_cards)  #LOG
 
     return render_template('question.html', prompt_num=prompt_num, prompt=prompt, next_prompt_num=next_prompt_num,
                            drawn_cards=drawn_cards)
 
-
+# reset function
 @app.route('/reset_game')
 def reset_game():
     # Clear session data related to the game
@@ -148,7 +161,7 @@ def reset_game():
     # Redirect back to the initial state of the game
     return redirect('/question')  # Redirect to the initial question route or wherever the game starts
 
-
+# REMOVE???? idk if this is used
 @app.route('/collect_answers', methods=['POST'])
 def collect_answers():
     prompt = request.form.get('prompt')  # Get the prompt from the form
@@ -170,9 +183,10 @@ def collect_answers():
     # Redirect to the next question prompt route
     return redirect(url_for('display_question', prompt_num=next_prompt_num))
 
-
+# runs when game ends; calculates humor score and displays data
 @app.route('/end_game')
 def end_game(game_data):
+    # emotion recognition model
     model = AutoModelForSequenceClassification.from_pretrained(
         "bdotloh/distilbert-base-uncased-go-emotion-empathetic-dialogues-context-v2")
 
@@ -187,10 +201,12 @@ def end_game(game_data):
     game_data_with_scores = [{'prompt': prompt, 'user_answer': user_answer, 'emotion_score': score}
                              for prompt, user_answer, score in zip(prompts, user_answers, humor_scores)]
 
+    # calculates total score of all questions
     total_score = 0
     for prompt, user_answer, score in zip(prompts, user_answers, humor_scores):
         total_score += score
 
+    # updates user stats
     update_user_stats(session['username'], total_score)
 
     # Clear game_data after calculating scores
@@ -200,12 +216,12 @@ def end_game(game_data):
     return [game_data_with_scores, total_score]
     # return game_data_with_scores
 
-
+# display instructions page
 @app.route('/instructions')
 def instructions():
     return render_template('instructions.html')
 
-
+# update user stats after a game
 def update_user_stats(username, total_score):
     conn = sqlite3.connect('user_stats.db')
     cursor = conn.cursor()
@@ -280,7 +296,7 @@ def profile():
     return render_template('profile_page.html', username=session['username'], games_played=games_played,
                            average_score=average_score, total_score=total_score, past_scores=past_scores)
 
-
+# display about page
 @app.route('/about')
 def about():
     return render_template('aboutUs.html')
